5f6b9ab395e74984f6702c71f381893ebf0212e2
Johan Baaij <johanbaaij@gmail.com>
2020-04-11 20:58:31 +1000

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,74 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Johan Baaij - software development',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Personal website of Johan Baaij'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+    // Doc: https://typescript.nuxtjs.org/
+    '@nuxt/typescript-build'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    // Doc https://github.com/Developmint/nuxt-webfontloader
+    'nuxt-webfontloader'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend() {}
+  },
+  webfontloader: {
+    google: {
+      families: ['Rubik']
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <div class="mt-8 grid grid-cols-12 gap-4">
+      <div class="col-start-4 col-span-1">
+        <img
+          class="w-10 ml-auto mt-4"
+          src="~/assets/img/logo.svg"
+          alt="Logo for Johan Baaij Software Development"
+        />
+      </div>
+      <div class="col-span-7">
+        <h1 class="lowercase text-5xl">
+          Johan Baaij<br />software development
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
