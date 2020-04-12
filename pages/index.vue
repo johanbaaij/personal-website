7b1ac39f2fc7e54e3e36743fcec2f333fd275a7f
Johan Baaij <johanbaaij@gmail.com>
2020-04-12 11:52:44 +1000

    feat: netlify CMS configuration

--- pages/index.vue
+++ pages/index.vue
@@ -16,3 +16,16 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  }
+})
+</script>
