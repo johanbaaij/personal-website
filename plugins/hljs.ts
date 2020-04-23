24a56fc958f98ee04e5cf8fa343c10d10b92d1b7
Johan Baaij <johanbaaij@gmail.com>
2020-04-23 22:00:31 +1000

    fix: import shell and json files for highlight.js

--- plugins/hljs.ts
+++ plugins/hljs.ts
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
 import xml from 'highlight.js/lib/languages/xml'
+import shell from 'highlight.js/lib/languages/shell'
+import json from 'highlight.js/lib/languages/json'
+
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('xml', xml)
+hljs.registerLanguage('shell', shell)
+hljs.registerLanguage('json', json)
 
 Vue.directive('highlightjs', {
   inserted(el) {
